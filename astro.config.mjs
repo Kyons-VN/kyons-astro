5946bdbafe5d549cbaa05a0e3004561c274d69d9
mibi <binhhm2009@gmail.com>
2023-08-24 09:03:53 +0700

    Add dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,25 @@
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import robotsTxt from 'astro-robots-txt';
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://kyons.vn',
+  integrations: [
+    tailwind(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'vi',
+        locales: {
+          vi: 'vi-VN',
+          en: 'en-US',
+        },
+      },
+    }),
+    robotsTxt(),
+    preact(),
+  ],
+});
